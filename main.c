3b7005a9e601876a43c795b64b3ee86d5ee59cd4
Heng Li <lh3@me.com>
2015-05-06 14:31:14 -0400

    r44: start versioning

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <ctype.h>
 
+#define BGT_VERSION "r44"
+
 int main_ucf2bgt(int argc, char *argv[]);
 int main_view(int argc, char *argv[]);
 
@@ -12,6 +14,7 @@ static int usage()
 	fprintf(stderr, "Commands:\n");
 	fprintf(stderr, "  import       convert unary VCF to BGT\n");
 	fprintf(stderr, "  view         extract from BGT\n");
+	fprintf(stderr, "  version      show version number\n");
 	return 1;
 }
 
@@ -19,9 +22,12 @@ int main(int argc, char *argv[])
 {
 	if (argc < 2) return usage();
 	if (strcmp(argv[1], "ucf2bgt") == 0) return main_ucf2bgt(argc-1, argv+1);
-	if (strcmp(argv[1], "import") == 0) return main_ucf2bgt(argc-1, argv+1);
-	if (strcmp(argv[1], "view") == 0) return main_view(argc-1, argv+1);
-	else {
+	else if (strcmp(argv[1], "import") == 0) return main_ucf2bgt(argc-1, argv+1);
+	else if (strcmp(argv[1], "view") == 0) return main_view(argc-1, argv+1);
+	else if (strcmp(argv[1], "version") == 0) {
+		puts(BGT_VERSION);
+		return 0;
+	} else {
 		fprintf(stderr, "[E::%s] unrecognized command '%s'\n", __func__, argv[1]);
 		return 1;
 	}
