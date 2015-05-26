8866a3c589b49ddd304e181e160ba7247ab62d70
Heng Li <lh3@me.com>
2015-05-26 09:41:52 -0400

    r110: subsampling with an expression

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 
-#define BGT_VERSION "r108"
+#define BGT_VERSION "r110"
 
 int main_ucf2bgt(int argc, char *argv[]);
 int main_view(int argc, char *argv[]);
--- view.c
+++ view.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include "bgt.h"
 #include "kexpr.h"
+#include "fmf.h"
 
 void *bed_read(const char *fn);
 void bed_destroy(void *_h);
@@ -22,15 +23,36 @@ static inline int read1(bgt_t *bgt, bgtm_t *bm, bcf1_t *b)
 static int filter_func(bcf_hdr_t *h, bcf1_t *b, int an, int ac, int n, const int *sidx, uint8_t *a[2], void *data)
 {
 	flt_aux_t *flt = (flt_aux_t*)data;
-	int64_t vi;
-	double vr;
-	int is_int, err;
+	int is_true, err;
 	if (flt->ke == 0) return 0;
 	ke_set_real(flt->ke, "AN", an);
 	ke_set_real(flt->ke, "AC", ac);
-	err = ke_eval(flt->ke, &vi, &vr, &is_int);
+	is_true = !!ke_eval_int(flt->ke, &err);
 	if (err) return 0;
-	return !vi;
+	return !is_true;
+}
+
+static char **get_samples(const char *expr, const fmf_t *fmf, int *_n)
+{
+	int err, i, n, m;
+	char **s;
+	kexpr_t *ke;
+	*_n = 0;
+	if (*expr != '?') return hts_readlines(expr, _n);
+	ke = ke_parse(expr+1, &err);
+	if (err) return 0;
+	for (i = n = m = 0, s = 0; i < fmf->n_rows; ++i) {
+		if (fmf_test(fmf, i, ke)) {
+			if (n == m) {
+				m = m? m<<1 : 16;
+				s = (char**)realloc(s, m * sizeof(char*));
+			}
+			s[n++] = strdup(fmf->rows[i].name);
+		}
+	}
+	s = (char**)realloc(s, n * sizeof(char*));
+	*_n = n;
+	return s;
 }
 
 int main_view(int argc, char *argv[])
@@ -40,7 +62,8 @@ int main_view(int argc, char *argv[])
 	bgtm_t *bm = 0;
 	bcf1_t *b;
 	htsFile *out;
-	char modew[8], *reg = 0, **samples = 0;
+	fmf_t *fmf = 0;
+	char modew[8], *reg = 0, *sexpr, **samples = 0, *tmpfn;
 	flt_aux_t flt;
 	void *bed = 0;
 
@@ -51,7 +74,7 @@ int main_view(int argc, char *argv[])
 		if (c == 'b') out_bcf = 1;
 		else if (c == 'r') reg = optarg;
 		else if (c == 'l') clevel = atoi(optarg);
-		else if (c == 's') samples = hts_readlines(optarg, &n_samples);
+		else if (c == 's') sexpr = optarg;
 		else if (c == 'e') excl = 1;
 		else if (c == 'B') bed = bed_read(optarg);
 		else if (is_multi && c == 'a') multi_flag |= BGT_F_SET_AC;
@@ -82,6 +105,13 @@ int main_view(int argc, char *argv[])
 		return 1;
 	}
 
+	tmpfn = (char*)malloc(strlen(argv[optind]) + 5);
+	sprintf(tmpfn, "%s.spl", argv[optind]);
+	fmf = fmf_read(tmpfn);
+	free(tmpfn);
+
+	if (sexpr) samples = get_samples(sexpr, fmf, &n_samples);
+
 	if (!is_multi) {
 		bgt = bgt_open(argv[optind]);
 		if (n_samples > 0) bgt_set_samples(bgt, n_samples, samples);
@@ -109,6 +139,7 @@ int main_view(int argc, char *argv[])
 		vcf_write1(out, bgt? bgt->h_out : bm->h_out, b);
 	bcf_destroy1(b);
 
+	fmf_destroy(fmf);
 	hts_close(out);
 	if (bgt) bgt_close(bgt);
 	if (bm) bgtm_close(bm);
